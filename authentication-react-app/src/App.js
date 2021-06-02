f3c099164c325aa6950368a618e65c0b63109cbe
Fojan Babaali <fozhanmail@gmail.com>
2021-06-02 15:56:01 -0700

    Update App.js

--- authentication-react-app/src/App.js
+++ authentication-react-app/src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
-import React ,{useState} from 'react';
+import React from "react";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Preferences from "./components/Preferences/Preferences";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
+
 function App() {
-  const [token, setToken] = useState();
-  if(!token){
-    return <Login setToken={setToken}/>
+
+  const token = getToken();
+  if (!token) {
+    return <Login setToken={setToken} />;
   }
 
   return (
