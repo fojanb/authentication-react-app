16de75b4d315af2b99a52ce200c86d27373cc2fa
Fojan Babaali <fozhanmail@gmail.com>
2021-06-02 11:36:43 -0700

    add

--- authentication-react-app/src/App.js
+++ authentication-react-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LoginButton from "./components/LoginButton/LoginButton";
+import LogoutButton from "./components/LogoutButton/LogoutButton";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginButton />
+      <LogoutButton />
     </div>
   );
 }
